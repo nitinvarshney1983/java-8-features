2f15f0f0d53500df95d1a7c8fbdddbf2387cdbe1
nitinvarshney1983 <nitinvarshneykiet@gmail.com>
2022-11-26 19:58:37 +0530

    added packaging structure

---
+++ src/main/java/com/techwhisky/java8features/exampe/App.java
@@ -0,0 +1,13 @@
+package com.techwhisky.java8features.exampe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/techwhisky/java8features/exampe/AppTest.java
@@ -0,0 +1,20 @@
+package com.techwhisky.java8features.exampe;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
